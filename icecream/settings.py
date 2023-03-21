413975776fdba8a79b7046426fb2f31f6206967f
Pouria Sal <27398502+Pouria-Salekani@users.noreply.github.com>
2023-03-20 21:23:29 -0700

    debugging

--- icecream/settings.py
+++ icecream/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g3-*p8e1pjd&)49f8k5uutpge36)f#g8=vl38%qc#o@1c^**2e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-d55a.up.railway.app']
 
