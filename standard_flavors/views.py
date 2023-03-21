6991b359bfc0ef0ee6da4495975ebdfa564664fa
Pouria Sal <27398502+Pouria-Salekani@users.noreply.github.com>
2023-03-20 22:07:45 -0700

    testing a better hashing

--- standard_flavors/views.py
+++ standard_flavors/views.py
@@ -9,19 +9,30 @@ import json
 def grab_flavors(file):
     data = json.loads(file)
     one_off = True
+    Standard_Flavors.objects.all().delete()
+
+    # for item in data:
+    #     #offset the error
+    #     if item == 'standard_flavors':
+    #         for std in data[item]:
+    #             flavor_name = std
+    #             if not Standard_Flavors.objects.filter(name=flavor_name).exists():
+    #                 print(flavor_name, ' -----DOES NOT EXIST')
+    #                 if one_off:
+    #                     Standard_Flavors.objects.all().delete()
+    #                     one_off = False
+    #                 flavor = Standard_Flavors(name=flavor_name)
+    #                 flavor.save()
 
     for item in data:
         #offset the error
         if item == 'standard_flavors':
             for std in data[item]:
                 flavor_name = std
-                if not Standard_Flavors.objects.filter(name=flavor_name).exists():
-                    print(flavor_name, ' -----DOES NOT EXIST')
-                    if one_off:
-                        Standard_Flavors.objects.all().delete()
-                        one_off = False
-                    flavor = Standard_Flavors(name=flavor_name)
-                    flavor.save()
+                flavor = Standard_Flavors(name=flavor_name)
+                flavor.save()
+
+
     
                 #flavor = Standard_Flavors.objects.get(name=flavor_name)
                 #update the name
